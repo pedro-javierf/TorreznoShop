ac51f20ba46d37a2037235be57ae45ec448e10ca
Tvring <55629023+Tvring@users.noreply.github.com>
2020-02-09 11:56:39 +0100

    Package created and some improvements

---
+++ Torrezno/Torrezno.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Torrezno {
+
+	//private int unitCost;
+	private Pigtype torreznoType;
+	private int deliciousness;
+	
+	
+	public Torrezno(Pigtype type, int delic)
+	{
+		torreznoType=type;
+		deliciousness=delic;
+	}
+	
+	public int getPrice()
+	{
+		return torreznoType.getCost();
+	}
+	
+	public int getDeliciousness()
+	{
+		return deliciousness;
+	}
+	
+}
